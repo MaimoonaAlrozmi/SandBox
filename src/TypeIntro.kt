eaf0e295ad8570810c2ca9ab8926b822f3de6ea2
MaiMoOna <memo38605@gmail.com>
2020-08-30 06:30:18 +0300

    my frist app in kotlin commit init

---
+++ src/TypeIntro.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var experiencePoints: Int = 10;
+    println(experiencePoints);
+
+    var playerName = "Estragon";
+    println(playerName);
+
+    var magic = playerName.reversed()
+    println(magic)
+
+    var hasSteed = false
+
+    if (hasSteed == false) {
+        println("They do not have dragon ^~^")
+
+    } else {
+        println("They have dragon ^_^")
+    }
+}
